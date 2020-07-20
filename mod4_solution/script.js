52e6861042cf2ef4238cfc3523588c497cde290b
Sara P <spersau@stevens.edu>
2020-07-20 13:39:21 -0400

    mod4

--- mod4_solution/script.js
+++ mod4_solution/script.js
@@ -4,7 +4,7 @@
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   for (var i = 0; i < names.length; i++) {
     
-    var firstLetter = ((names[i]).charAt(0).toLowerCase();
+    var firstLetter = ((names[i]).charAt(0).toLowerCase());
     if (firstLetter === 'j') {
       
       window.byeSpeaker.speak(names[i]);
