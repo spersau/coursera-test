332438e22c4f13f43a17fd262848880ea0c71d90
Sara P <spersau@stevens.edu>
2020-07-20 12:03:13 -0400

    mod4

--- mod4_solution/script.js
+++ mod4_solution/script.js
@@ -1,20 +1,20 @@
 
-(function () {
+(function (window) {
 
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   for (var i = 0; i < names.length; i++) {
     
-    var firstLetter = names[i].charAt(0).toLowerCase();
+    var firstLetter = ((names[i]).charAt(0).toLowerCase();
     if (firstLetter === 'j') {
       
-      byeSpeaker.speak(names[i]);
+      window.byeSpeaker.speak(names[i]);
 
   } else {
     
-      helloSpeaker.speak(names[i]);
+      window.helloSpeaker.speak(names[i]);
 
   }
 
 }
 
-})();
+})(window);
